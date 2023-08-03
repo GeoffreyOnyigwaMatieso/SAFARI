a0ce969d0d93259c2283efce0735b2295624dec2
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-08-03 22:33:18 +0300

    Added live charts

--- viewBus.php
+++ viewBus.php
@@ -299,11 +299,19 @@ button
 ?>
 
 
-
-
-
-
 </div>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64cbfceb94cf5d49dc684715/1h6ud7v4t';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
